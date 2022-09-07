86549f8574d2fedd92bc6ae0551e9004d258735b
ROmen <manuk_1122@mail.ru>
2022-09-07 11:15:17 +0300

    изменил 6 строку

---
+++ rure.py
@@ -0,0 +1,5 @@
+import turtle
+window = turtle.Screen()
+window.setup(800,600)
+
+turtle.exitonclick()
---
+++ turtle.py
@@ -0,0 +1,6 @@
+import turtle
+window = turtle.screen()
+window.setup(800,600)
+
+turtle.exitonclick()
+gffxfx7777058714
