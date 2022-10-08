7f3112838454d0696b860a00238983072fe9192d
ROmen <manuk_1122@mail.ru>
2022-10-08 12:43:01 +0300

    печать слова по буквам

--- wordMix/game.py
+++ wordMix/game.py
@@ -1,3 +1,13 @@
 file = open('Words.txt','r')
-print(*file)
-print(file)
+#print(*file)
+#print(file)
+#for i in range(5):
+ #   print(file.read(i))
+with open("Words.txt") as file:
+    print(file.readline(1))
+    print(file.readline(1))
+    print(file.readline(1))
+    print(file.readline(1))
+    print(file.readline(1))
+    print(file.readline(1))
+file.close()
