abce2953cca2ccbbe5ff9d737d4e3bfdc857bbcc
ROmen <manuk_1122@mail.ru>
2022-10-19 15:14:21 +0300

    дописали программу

--- wordMix/game.py
+++ wordMix/game.py
@@ -1,13 +1,23 @@
-file = open('Words.txt','r')
-#print(*file)
-#print(file)
-#for i in range(5):
- #   print(file.read(i))
-with open("Words.txt") as file:
-    print(file.readline(1))
-    print(file.readline(1))
-    print(file.readline(1))
-    print(file.readline(1))
-    print(file.readline(1))
-    print(file.readline(1))
-file.close()
+from ast import For
+import random
+import os
+file = open('Words.txt',encoding='utf-8',mode="r")
+for i in range(random.randint(0,3)):
+    file.readline()
+txt=file.readline()
+txt = txt.replace('\n','')
+Word=txt.split()
+for i, word in enumerate(map(list,Word)):
+    random.shuffle(word)
+    Word[i]=''.join(word)
+attempt=5
+while(True):
+    os.system('cls||clear')
+    print(Word)
+    answ=input('введите слово')
+    if(answ==txt):
+        print('вы угадали')
+        breaк
+    else:
+        print('вы не угадали')                 
+        break
