b08e9d57a08b8d19054d52ad752f38e10a659bc2
ROmen <manuk_1122@mail.ru>
2022-10-22 13:16:13 +0300

    иссправили баги и ошики

--- wordMix/game.py
+++ wordMix/game.py
@@ -1,23 +1,32 @@
 from ast import For
 import random
 import os
-file = open('Words.txt',encoding='utf-8',mode="r")
-for i in range(random.randint(0,3)):
-    file.readline()
-txt=file.readline()
-txt = txt.replace('\n','')
-Word=txt.split()
-for i, word in enumerate(map(list,Word)):
-    random.shuffle(word)
-    Word[i]=''.join(word)
-attempt=5
 while(True):
-    os.system('cls||clear')
-    print(Word)
-    answ=input('введите слово')
-    if(answ==txt):
-        print('вы угадали')
-        breaк
-    else:
-        print('вы не угадали')                 
-        break
+    file = open('Words.txt',encoding='utf-8',mode="r")
+    for i in range(random.randint(0,19)):
+        file.readline()
+    txt=file.readline()
+    txt = txt.replace('\n','')
+    Word=txt.split()
+    for i, word in enumerate(map(list,Word)):
+        random.shuffle(word)
+        Word[i]=''.join(word)
+    attempt=5
+    while(True):
+        os.system('cls||clear')
+        print(Word)
+        answ=input('введите слово  ')
+        if(answ==txt):
+            print(f'вы угадали с {6-attempt}-й попытки')
+            input('нажмите enter')
+            break
+            file.close()
+        elif(attempt>1):
+            print('попытайтесь ещё') 
+            attempt-=1
+            print(f'у вас осталось {attempt} попыток')
+            input('нажмите enter')   
+        else:
+            print('вы не угадали')                 
+            break
+            file.close()
