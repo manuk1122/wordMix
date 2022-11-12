3049a9f4a3ec005648817741512e62fc0406c257
ROmen <manuk_1122@mail.ru>
2022-11-12 11:26:52 +0300

    добавили кнопку и те

--- wordMix/game.py
+++ wordMix/game.py
@@ -3,16 +3,18 @@ from difflib import Match
 import random
 import os
 from tkinter import *      
-
+from PIL import Image
 ws = Tk()
 ws.title('PythonGuides')
-ws.geometry('500x500')
+ws.geometry('1920x1080')
 
-#ws.configure(bg='black')
-canvas = Canvas(ws,width = 512, height = 512)
-
+ws.configure(bg='black')
+canvas = Canvas(ws,width = 150, height = 150)
 canvas.pack()      
-img = PhotoImage(file='logo.png') 
+img_new=Image.open('logo.png')
+img_new1=img_new.resize((150,150)) 
+img_new1.save('logo1.png')
+img = PhotoImage(file='logo1.png')
 canvas.place(x=0,y=0)     
 canvas.create_image(
     0,
@@ -20,6 +22,26 @@ canvas.create_image(
     anchor=NW, 
     image=img
     )      
+#кнопка
+button = Button(
+    text="ввод!",
+    width=25,
+    height=5,
+    bg="yellow",
+    fg="black",
+)
+ 
+button.pack()
+button.place(x=1500,y=700)
+#------------------------------------------------
+text_box = Text(
+    width=50,
+    height=5,
+    bg="yellow",
+    fg="black",
+)
+text_box.pack()
+text_box.place(x=810,y=700) 
 ws.mainloop()  
 """while(True):
     answ=input('введите сложность 1.легко 2.сложно ')
